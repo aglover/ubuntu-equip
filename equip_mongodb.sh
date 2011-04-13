69ec95fef90d484e9b53dc414121b21be3e69de1
Andrew Glover <ajglover@gmail.com>
2011-04-13 17:01:28 -0400

    fixed mongo source list issue

--- equip_mongodb.sh
+++ equip_mongodb.sh
@@ -10,8 +10,7 @@ sudo apt-get install libpcre++-dev libboost-dev libreadline-dev xulrunner-1.9.1-
 sudo apt-get install libboost-program-options-dev libboost-thread-dev libboost-filesystem-dev libboost-date-time-dev -y
 
 
-# sudo add-apt-repository "deb http://downloads.mongodb.org/distros/ubuntu  10.4 10gen"
-sudo echo "deb http://downloads.mongodb.org/distros/ubuntu  10.4 10gen" > /etc/apt/sources.list
+sudo sh -c 'echo "deb http://downloads.mongodb.org/distros/ubuntu  10.4 10gen" > /etc/apt/sources.list.d/mongo-stable.list'
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 sudo apt-get update -y
