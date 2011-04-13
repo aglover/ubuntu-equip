129705ef5ac6fc46448fbc008af1db00b3b8914f
Andrew Glover <ajglover@gmail.com>
2011-04-13 16:20:30 -0400

    fixed url of mongo

--- equip_mongodb.sh
+++ equip_mongodb.sh
@@ -10,7 +10,7 @@ sudo apt-get install libpcre++-dev libboost-dev libreadline-dev xulrunner-1.9.1-
 sudo apt-get install libboost-program-options-dev libboost-thread-dev libboost-filesystem-dev libboost-date-time-dev -y
 
 
-sudo add-apt-repository "deb http://downloads.mongodb.org/distros/ubuntu  9.10 10gen"
+sudo add-apt-repository "deb http://downloads.mongodb.org/distros/ubuntu  10.4 10gen"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 sudo apt-get update -y
 sudo apt-get install mongodb-stable -y
