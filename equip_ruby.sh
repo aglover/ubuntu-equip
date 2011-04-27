a1db8e5f1b05f9c87e2ba4f7d2a9a7c6aa8d961f
Andrew Glover <ajglover@gmail.com>
2011-04-27 09:53:43 -0400

    put sudos back

--- equip_ruby.sh
+++ equip_ruby.sh
@@ -5,25 +5,25 @@
 #  Ruby Equip
 # Licence: MIT
 
-apt-get update
-apt-get install build-essential rsync telnet screen man -y
-apt-get install strace tcpdump -y
+sudo apt-get update
+sudo apt-get install build-essential rsync telnet screen man -y
+sudo apt-get install strace tcpdump -y
 apt-get install libssl-dev zlib1g-dev libcurl3-dev libxslt-dev -y
-apt-get install zlib1g-dev libreadline5-dev -y
-apt-get install gcc g++ linux-headers-generic libsqlite3-dev -y
+sudo apt-get install zlib1g-dev libreadline5-dev -y
+sudo apt-get install gcc g++ linux-headers-generic libsqlite3-dev -y
 
-apt-get install git -y
+sudo apt-get install git -y
 
-apt-get install libruby1.9 -y
-wget ftp://ftp.ruby-lang.org//pub/ruby/1.9/ruby-1.9.2-p180.tar.gz
-tar xzf ruby-1.9.2-p180.tar.gz
+sudo apt-get install libruby1.9 -y
+sudo wget ftp://ftp.ruby-lang.org//pub/ruby/1.9/ruby-1.9.2-p180.tar.gz
+sudo tar xzf ruby-1.9.2-p180.tar.gz
 cd ruby-1.9.2-p180
-./configure
-make
-make install
-make install-doc
+sudo ./configure
+sudo make
+sudo make install
+sudo make install-doc
 
 
-gem update --system
+sudo gem update --system
 
-gem install bundler 
+sudo gem install bundler 
