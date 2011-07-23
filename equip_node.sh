e29c0cae5980a05d1a0bcd73037b40b323fb7f67
Andrew Glover <ajglover@gmail.com>
2011-07-23 09:02:08 -0400

    Added node.js script

--- equip_node.sh
+++ equip_node.sh
@@ -7,21 +7,7 @@
 
 
 sudo apt-get update
-sudo apt-get install build-essential rsync telnet screen man -y
-sudo apt-get install strace tcpdump -y
-sudo apt-get install libssl-dev zlib1g-dev libcurl3-dev libxslt-dev -y
-sudo apt-get install zlib1g-dev libreadline5-dev -y
-sudo apt-get install gcc g++ linux-headers-generic libsqlite3-dev -y
-
-sudo apt-get install git -y
-
-git clone --depth 1 git://github.com/joyent/node.git
-cd node
-git checkout origin/v0.4 
-mkdir ~/local
-./configure --prefix=$HOME/local/node
-make
-make install
-echo 'export PATH=$HOME/local/node/bin:$PATH' >> ~/.profile
-echo 'export NODE_PATH=$HOME/local/node:$HOME/local/node/lib/node_modules' >> ~/.profile
-source ~/.profile
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
