a6bc96ea741e8f9d69e9ec9077cc81036614cde9
Andrew Glover <ajglover@gmail.com>
2011-12-26 18:00:45 -0500

    added apache 2 equip

---
+++ equip_apache.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#
+# Ubuntu Equip 
+#  Apache + base
+# Licence: MIT
+
+wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
+
+sudo apt-get install apache2
+sudo apt-get install apache2-common
