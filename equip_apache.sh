af582aa3339782be94833feb77987faf532753ee
Andrew Glover <ajglover@gmail.com>
2011-12-26 22:48:41 -0500

    updated links in apache

--- equip_apache.sh
+++ equip_apache.sh
@@ -4,6 +4,7 @@
 # Ubuntu Equip 
 #  Apache + base
 # Licence: MIT
+# notes for ubuntu https://help.ubuntu.com/8.04/serverguide/C/httpd.html
 
 wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
 
