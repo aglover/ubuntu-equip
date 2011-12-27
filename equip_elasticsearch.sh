18e8f8e310a2a66c69009093170ddd7ec1564909
Andrew Glover <ajglover@gmail.com>
2011-12-27 09:40:09 -0500

    added yml for elastic search

--- equip_elasticsearch.sh
+++ equip_elasticsearch.sh
@@ -13,10 +13,15 @@ sudo apt-get install unzip -y
 
 curl -OL https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.18.6.zip
 unzip elasticsearch-* && rm elasticsearch-*.zip
-sudo mv elasticsearch-* /usr/local/elasticsearch
+mv elasticsearch-0.18.6/ elasticsearch/
+sudo mv elasticsearch/ /usr/local/elasticsearch
 
 wget --no-check-certificate https://raw.github.com/aglover/ubuntu-equip/master/etc/elasticsearch.init.d
 
 sudo mv ./elasticsearch.init.d /etc/init.d/elasticsearch
 sudo chmod +x /etc/init.d/elasticsearch
 sudo update-rc.d elasticsearch defaults
+
+wget --no-check-certificate https://raw.github.com/aglover/ubuntu-equip/master/etc/elasticsearch.yml
+
+sudo ./elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
