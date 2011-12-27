41919a1007f078e166ff83c050062b1fd2d46d1e
Andrew Glover <ajglover@gmail.com>
2011-12-26 20:41:38 -0500

    working on redis install

--- equip_redis.sh
+++ equip_redis.sh
@@ -8,25 +8,12 @@
 wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
 
 sudo apt-get update -y
+sudo apt-get install tcl8.5
 
-wget http://redis.googlecode.com/files/redis-2.2.4.tar.gz
-tar -zxf redis-2.2.4.tar.gz
-cd redis-2.2.4
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+
 make
-sudo make install
+make test
 
-cd ~
-
-wget https://github.com/ijonas/dotfiles/raw/master/etc/init.d/redis-server
-sudo mv redis-server /etc/init.d/redis-server
-sudo chmod +x /etc/init.d/redis-server
-
-sudo mkdir -p /var/lib/redis
-sudo mkdir -p /var/log/redis
-sudo useradd --system --home-dir /var/lib/redis redis
-
-sudo chown redis.redis /var/lib/redis
-sudo chown redis.redis /var/log/redis
-
-sudo update-rc.d redis-server defaults
-sudo /etc/init.d/redis-server start
