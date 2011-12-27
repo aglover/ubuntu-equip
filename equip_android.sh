d2bf0f82e8a6debaa57f21e5936fcd03280f50ff
Andrew Glover <ajglover@gmail.com>
2011-12-26 19:37:04 -0500

    added android installer

--- equip_android.sh
+++ equip_android.sh
@@ -13,3 +13,4 @@ wget --no-check-certificate http://dl.google.com/android/android-sdk_r16-linux.t
 
 gunzip ./android-sdk_r16-linux.tgz
 tar xvf ./android-sdk_r16-linux.tar
+./android-sdk-linux/tools/android update sdk --no-ui
