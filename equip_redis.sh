bd0fe293b3d3433c9847c338fc75c3b0b8e42ad8
Andrew Glover <ajglover@gmail.com>
2011-12-27 09:00:04 -0500

    updates to redis installer

--- equip_redis.sh
+++ equip_redis.sh
@@ -15,8 +15,9 @@ wget http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz
 cd redis-stable
 
-make install
+make 
 make test
+sudo make install
 
 wget --no-check-certificate https://raw.github.com/aglover/ubuntu-equip/master/etc/redis.conf
 sudo mv redis.conf /etc/redis.conf
@@ -24,6 +25,13 @@ sudo mv redis.conf /etc/redis.conf
 wget https://github.com/ijonas/dotfiles/raw/master/etc/init.d/redis-server
 sudo mv redis-server /etc/init.d/redis-server
 sudo chmod +x /etc/init.d/redis-server
+
+sudo mkdir -p /var/lib/redis
+sudo mkdir -p /var/log/redis
+sudo useradd --system --home-dir /var/lib/redis redis
+sudo chown redis.redis /var/lib/redis
+sudo chown redis.redis /var/log/redis
+
 
 sudo update-rc.d redis-server defaults
 sudo /etc/init.d/redis-server start
