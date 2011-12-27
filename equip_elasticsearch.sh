02606e219d88b9a25f5b4399d464c31e667b7c46
Andrew Glover <ajglover@gmail.com>
2011-12-27 10:00:46 -0500

    updated header

--- equip_elasticsearch.sh
+++ equip_elasticsearch.sh
@@ -2,7 +2,7 @@
 
 #
 # Ubuntu Equip 
-#  Redis Equip
+#  elasticsearch Equip
 # Licence: MIT
 
 wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
