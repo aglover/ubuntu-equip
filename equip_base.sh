55bc0c7ca022fc1f5ce790cc77548fe5d0ff2fc1
Andrew Glover <aglover@beacon50.com>
2013-01-21 10:51:17 -0500

    updated base to install python stuff

--- equip_base.sh
+++ equip_base.sh
@@ -10,5 +10,6 @@ sudo apt-get update
 sudo apt-get install build-essential rsync telnet screen man -y
 sudo apt-get install strace tcpdump -y
 sudo apt-get install libssl-dev zlib1g-dev libcurl3-dev libxslt-dev -y
+sudo apt-get install python-software-properties -y
 
 sudo apt-get install git -y
