f5d3b05da246f354a55cad8b0c1fa500bf381aad
Andrew Glover <aglover@beacon50.com>
2013-05-17 17:10:08 -0400

    removed some stuf that wasn't working

--- equip_elasticsearch.sh
+++ equip_elasticsearch.sh
@@ -15,17 +15,17 @@ sudo apt-get install unzip -y
 curl -OL https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.0.zip
 unzip elasticsearch-* && rm -f elasticsearch-*.zip
 mv elasticsearch-0.90.0/ elasticsearch/
-sudo mv elasticsearch/ /usr/local/elasticsearch
+# sudo mv elasticsearch/ /usr/local/elasticsearch
 
-wget --no-check-certificate https://raw.github.com/aglover/ubuntu-equip/master/etc/elasticsearch.init.d
+# wget --no-check-certificate https://raw.github.com/aglover/ubuntu-equip/master/etc/elasticsearch.init.d
 
-sudo mv ./elasticsearch.init.d /etc/init.d/elasticsearch
-sudo chmod +x /etc/init.d/elasticsearch
-sudo update-rc.d elasticsearch defaults
+# sudo mv ./elasticsearch.init.d /etc/init.d/elasticsearch
+# sudo chmod +x /etc/init.d/elasticsearch
+# sudo update-rc.d elasticsearch defaults
 
-wget --no-check-certificate https://raw.github.com/aglover/ubuntu-equip/master/etc/elasticsearch.yml
+# wget --no-check-certificate https://raw.github.com/aglover/ubuntu-equip/master/etc/elasticsearch.yml
 
-sudo mkdir /etc/elasticsearch
-sudo mv ./elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+# sudo mkdir /etc/elasticsearch
+# sudo mv ./elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 
 # you will need to manually start elasticsearch via /etc/init.d/elasticsearch start
