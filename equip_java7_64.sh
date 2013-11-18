e1d91b1375b3a97737cf04210143ec546c1a5955
Andrew Glover <ajglover@gmail.com>
2013-11-18 08:30:42 -0500

    removed ant install

--- equip_java7_64.sh
+++ equip_java7_64.sh
@@ -8,8 +8,6 @@
 
 wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
 
-sudo apt-get install ant -y
-
 sudo apt-get install curl -y 
 
 curl -L --cookie "oraclelicensejdk-7u25-b15-oth-JPR=accept-securebackup-cookie;gpw_e24=http://edelivery.oracle.com" http://download.oracle.com/otn-pub/java/jdk/7u25-b15/jdk-7u25-linux-x64.tar.gz -o jdk-7-linux-x64.tar.gz
