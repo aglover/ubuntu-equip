f6f0c3006046921ef5cc0c81cb7ef1a30bdb8ef2
Andrew Glover <ajglover@gmail.com>
2013-11-18 08:27:20 -0500

    changed order of ant install

--- equip_java7_64.sh
+++ equip_java7_64.sh
@@ -8,6 +8,8 @@
 
 wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
 
+sudo apt-get install ant -y
+
 sudo apt-get install curl -y 
 
 curl -L --cookie "oraclelicensejdk-7u25-b15-oth-JPR=accept-securebackup-cookie;gpw_e24=http://edelivery.oracle.com" http://download.oracle.com/otn-pub/java/jdk/7u25-b15/jdk-7u25-linux-x64.tar.gz -o jdk-7-linux-x64.tar.gz
@@ -28,8 +30,6 @@ sudo chown -R root:root /usr/lib/jvm/jdk1.7.0
 rm jdk-7-linux-x64.tar.gz
 rm equip_base.sh
 rm equip_java7_64.sh
-
-sudo apt-get install ant -y
 
 java -version
 
