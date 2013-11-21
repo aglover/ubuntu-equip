de37c162053916b28984f56f4d0118284619a8af
Andrew Glover <ajglover@gmail.com>
2013-11-21 10:16:19 -0500

    more refactoring of android installer

--- equip_android.sh
+++ equip_android.sh
@@ -8,6 +8,5 @@
 wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
 wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_android_base.sh && bash equip_android_base.sh
 
-sudo apt-get install expect
 wget --no-check-certificate https://raw.github.com/aglover/ubuntu-equip/master/etc/android_expect_all.sh && bash android_expect_all.sh
 rm android_expect_all.sh
--- equip_android_base.sh
+++ equip_android_base.sh
@@ -5,9 +5,9 @@
 #  Android base install Equip
 # Licence: MIT
 
-wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
 sudo apt-get update -y
 wget --no-check-certificate http://dl.google.com/android/android-sdk_r22.3-linux.tgz
 tar -zxf android-sdk_r22.3-linux.tgz
 sudo mv android-sdk-linux /usr/local
 rm android-sdk_r22.3-linux.tgz
+sudo apt-get install expect -y
