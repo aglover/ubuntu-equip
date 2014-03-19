a5ccfc6a5c9507f412c30c5195dd3fd9c5ad4d0d
Marcello de Sales <Marcello_DeSales@intuit.com>
2014-03-19 16:35:33 -0700

    Adding support to Java8 equip
    This commit adds the support to Java8. The automation is based on the original Java
    support, plus some insights about how to accept the terms and conditions automatically
    as described by http://www.webupd8.org/2012/09/install-oracle-java-8-in-ubuntu-via-ppa.html
    
    The step is also included in the shell.

---
+++ equip_java8.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#
+# Ubuntu Equip 
+#  Java 8 Equip
+# Licence: MIT
+
+sudo echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update -y
+sudo apt-get install oracle-java8-installer -y
+sudo apt-get install ant -y
