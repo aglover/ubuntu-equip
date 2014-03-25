5c0788af92a46290ecf5ecdd949d2b368b3f0a3b
Andy Glover <aglover@netflix.com>
2014-03-25 11:30:35 -0700

    added some doc links to new java8 script

--- equip_java8.sh
+++ equip_java8.sh
@@ -4,7 +4,8 @@
 # Ubuntu Equip 
 #  Java 8 Equip
 # Licence: MIT
-
+# see http://www.webupd8.org/2012/09/install-oracle-java-8-in-ubuntu-via-ppa.html
+# http://stackoverflow.com/questions/13018626/add-apt-repository-not-found
 sudo echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
 
