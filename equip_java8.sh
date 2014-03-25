f9a57d06cc9fed3b2daca8bf16de62da0201ac63
Andy Glover <aglover@netflix.com>
2014-03-25 10:21:16 -0700

    updated base script to install wget and software-properties-common

--- equip_java8.sh
+++ equip_java8.sh
@@ -8,7 +8,7 @@
 sudo echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
 wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
 
-sudo add-apt-repository ppa:webupd8team/java
+sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update -y
 sudo apt-get install oracle-java8-installer -y
 sudo apt-get install ant -y
