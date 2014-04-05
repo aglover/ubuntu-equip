a4fb7fb8b01f12cfa173f685df7628de82377118
Andrew Glover <aglover@netflix.com>
2014-04-04 19:09:58 -0700

    added doc links to script

--- equip_docker.sh
+++ equip_docker.sh
@@ -4,7 +4,8 @@
 # Ubuntu Equip 
 #  Docker Equip
 # Licence: MIT
-
+# See http://docs.docker.io/en/latest/installation/ubuntulinux/ 
+#  & https://stackoverflow.com/questions/19412911/docker-installation-on-ubuntu-raring/19416387#19416387
 
 sudo sh -c "curl https://get.docker.io/gpg | apt-key add -"
 sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
