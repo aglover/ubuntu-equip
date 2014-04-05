a22f9829555846d6e3f286fcd18aead2748d2aef
Andrew Glover <aglover@netflix.com>
2014-04-04 19:04:16 -0700

    removed base stuff, not needed for now w/r to docker

--- equip_docker.sh
+++ equip_docker.sh
@@ -6,9 +6,7 @@
 # Licence: MIT
 
 
-wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
-
 sudo sh -c "curl https://get.docker.io/gpg | apt-key add -"
 sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 sudo apt-get update
-sudo apt-get install lxc-docker
+sudo apt-get install lxc-docker -y
