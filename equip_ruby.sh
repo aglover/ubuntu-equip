18ad510633c0e9715769130cde38051c8967dcb4
Andrew Glover <aglover@netflix.com>
2014-05-13 13:41:04 -0700

    added libyaml to Ruby installs

--- equip_ruby.sh
+++ equip_ruby.sh
@@ -9,7 +9,7 @@
 wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
 
 sudo apt-get install libssl-dev zlib1g-dev libcurl3-dev libxslt-dev zlib1g-dev libreadline-dev libsasl2-dev -y
-sudo apt-get install gcc g++ linux-headers-generic libsqlite3-dev -y
+sudo apt-get install gcc g++ linux-headers-generic libsqlite3-dev libyaml -y
 
 sudo apt-get install libruby1.9 -y
 sudo wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p448.tar.gz
--- equip_ruby_2_0.sh
+++ equip_ruby_2_0.sh
@@ -9,7 +9,7 @@
 wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
  
 sudo apt-get install libssl-dev zlib1g-dev libcurl3-dev libxslt-dev zlib1g-dev libreadline-dev libsasl2-dev -y
-sudo apt-get install gcc g++ linux-headers-generic libsqlite3-dev -y
+sudo apt-get install gcc g++ linux-headers-generic libsqlite3-dev libyaml -y
  
 sudo apt-get install libruby2.0 -y
 sudo wget http://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p353.tar.gz
