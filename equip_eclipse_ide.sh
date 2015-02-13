d09829ee5429bdcc2b1ac5ad1fce00aa4ba7d848
Renato Silva <resilva87@outlook.com>
2015-02-13 18:26:12 -0200

    Provisioning Eclipse IDE (https://eclipse.org/downloads/)

--- equip_eclipse_ide.sh
+++ equip_eclipse_ide.sh
@@ -6,10 +6,19 @@
 # Licence: MIT
 # to run: wget --no-check-certificate https://github.com/resilva87/ubuntu-equip/raw/master/equip_eclipse_ide.sh && bash equip_eclipse_ide.sh
 
-wget --no-check-certificate https://github.com/resilva87/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
+if [ -d "/opt/eclipse" ]; then
+	echo "Eclipse already installed in /opt/eclipse, skipping!"
+	exit 0
+fi
 
-# Download Eclipse IDE
-wget --no-check-certificate "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/luna/SR1a/eclipse-jee-luna-SR1a-linux-gtk-x86_64.tar.gz"
+VERSION="eclipse-jee-luna-SR1a-linux-gtk-x86_64.tar.gz"
+LINK="http://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/luna/SR1a/$VERSION"
+
+echo $VERSION
+echo $LINK
+
+wget --no-check-certificate https://github.com/resilva87/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh#
+wget --no-check-certificate "$LINK"
 
 mv eclipse-jee-luna-SR1a-linux-gtk-x86_64.tar.gz eclipse.tar.gz
 
