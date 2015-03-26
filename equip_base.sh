e731dea6350a664046825744aae3d794909d842e
Andrew Glover <ajglover@gmail.com>
2015-03-26 07:36:37 -0700

    adding equip_base back in as Ruby script needs it as well as a few others

---
+++ equip_base.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#
+# Ubuntu Equip 
+#  Equip basec/core libs
+# Licence: MIT
+
+sudo apt-get update -y
+
+sudo apt-get install build-essential rsync telnet screen man wget -y
+sudo apt-get install strace tcpdump -y
+sudo apt-get install libssl-dev zlib1g-dev libcurl3-dev libxslt-dev -y
+sudo apt-get install software-properties-common python-software-properties -y
+
+sudo apt-get install git -y
