74bf704542665ab82bc94ba1f1f1c5b9f6d44e73
Andrew Glover <ajglover@gmail.com>
2015-05-03 17:27:34 -0700

    added -y

--- equip_postgres.sh
+++ equip_postgres.sh
@@ -6,5 +6,5 @@
 #  https://www.digitalocean.com/community/tutorials/how-to-install-and-use-postgresql-on-ubuntu-14-04
 # Licence: MIT
 
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install postgresql postgresql-contrib -y
