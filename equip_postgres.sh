eec78b5e05b6256de37e6a783501df4eef1a9126
Andrew Glover <ajglover@gmail.com>
2015-05-03 17:24:17 -0700

    added postgres install script

---
+++ equip_postgres.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#
+# Ubuntu Equip 
+#  Postgres Install
+#  https://www.digitalocean.com/community/tutorials/how-to-install-and-use-postgresql-on-ubuntu-14-04
+# Licence: MIT
+
+sudo apt-get update
+sudo apt-get install postgresql postgresql-contrib -y
